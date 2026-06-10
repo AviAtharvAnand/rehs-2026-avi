7ba8c6eb4085f2a5105000b50c1b5bd4e4b63905
Avi Atharv Anand <avi.atharv.anand@gmail.com>
2026-06-10 15:09:55 -0700

    Tested image analysis of qwen3

---
+++ llm_testing.py
@@ -0,0 +1,29 @@
+# hello_llm.py
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+client = OpenAI(
+    api_key=os.environ["NRP_LLM_TOKEN"],
+    base_url=os.environ["NRP_LLM_BASE_URL"],
+)
+response = client.chat.completions.create(
+    model="qwen3",
+    messages=[
+        {"role": "user", 
+        
+            "content": [
+                {
+                    "type": "text",
+                    "text": "Describe the image provided in 20 words or less. \
+                    'image_url': 'https://stellwagen.noaa.gov/media/img/20210810-breach-behavior-1000.jpg'"
+                },
+            ]
+        }
+    
+    ],
+)
+
+
+print(response.choices[0].message.content)
