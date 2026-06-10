286d009f3e7da7c6020daec9041d03b85b63519e
Avi Atharv Anand <avi.atharv.anand@gmail.com>
2026-06-10 15:52:32 -0700

    testing gpt-oss reasoning capability

--- llm_testing.py
+++ llm_testing.py
@@ -8,21 +8,17 @@ client = OpenAI(
     base_url=os.environ["NRP_LLM_BASE_URL"],
 )
 response = client.chat.completions.create(
-    model="qwen3",
-    messages=[
-        {"role": "user", 
-        
-            "content": [
-                {
-                    "type": "text",
-                    "text": "Describe the image provided in 20 words or less. \
-                    'image_url': 'https://stellwagen.noaa.gov/media/img/20210810-breach-behavior-1000.jpg'"
-                },
-            ]
-        }
-    
-    ],
-)
+    model="gpt-oss",
+    messages=[{"role": "user", 
+        "content": "Three people A, B, and C each wear either a red or blue hat. They can see the others' \
+        hats but not their own. They are told: At least one of you has a red hat.\
+        A looks at B and C and says, I don’t know my hat color.\
+        B then says, I don’t know my hat color.\
+        C then says, I know my hat color.\
+        What color is C’s hat, and why?"
+    }],
 
+    extra_body = {"reasoning_effort": "high"}
+)
 
 print(response.choices[0].message.content)
