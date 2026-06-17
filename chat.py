bc9b65c2ea534c48c3b70bbeadf36c9a6a4c9df6
Avi Atharv Anand <avi.atharv.anand@gmail.com>
2026-06-17 14:55:57 -0700

    added history.json to .gitignore, addad a cleaner UI for chat.py, gave chat.py a history even when conversations are ended.

--- chat.py
+++ chat.py
@@ -1,6 +1,15 @@
+"""
+Create history.json to store the conversation history and
+add [{"role": "system", "content": "normal"}] to 
+history.json before running this code for the first time.
+"""
 import os
+import json
 from dotenv import load_dotenv
 from openai import OpenAI
+from rich import print
+from rich.panel import Panel
+
 MODEL = "gpt-oss"
 
 load_dotenv()
@@ -9,27 +18,32 @@ client = OpenAI(
     base_url = os.environ["NRP_LLM_BASE_URL"]
 )
 
-print("NRP chat. Press e to exit\n")
 
-content = input("Choose a style for your bot(ex. speak like a pirate): ")
-history = [
-        {"role": "system", "content": content},
-    ]
+print(Panel("[blue]NRP chat. Press e to exit"))
+
+with open('history.json', 'r') as f:
+    history = json.load(f)
+
 while True:
     question = input("You: ")
     print()
     user_input = {"role": "user", "content": question}
     history.append(user_input)
     if question == "e":
-        print("Bye!")
+        with open('history.json', 'w') as f:
+            json.dump(history, f)
+        print(Panel("[green]Bye!"))
         break
 
     response = client.chat.completions.create(
         model = MODEL,
         messages = history
     )
-    reply = {"role": "user", "content": response.choices[0].message.content}
+    reply = {"role": "assistant", "content": response.choices[0].message.content}
     history.append(reply)
 
-    print(f"{MODEL}: {response.choices[0].message.content}")
+    print(Panel(f"[green]{MODEL}: {response.choices[0].message.content}"))
     print()
+
+
+
